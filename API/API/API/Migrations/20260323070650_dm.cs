68e50060eb5d4e64f5477f86dd7eff3e8e94f374
thầy vàng <141602381+houkillDariky@users.noreply.github.com>
2026-03-24 16:54:29 +0700

    sửa Migrations

---
+++ API/API/API/Migrations/20260323070650_dm.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class dm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$AxkmOV0wyO7s5Zthv2Sbu.NvhTVTk0hzpI2BZLxYXiOW.sG0mZyVK");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$W5dyrBTt.oG27kvQFcX0ouMCinsEOqrpI5JpmnYkNqK1X447ztVR6");
+        }
+    }
+}
