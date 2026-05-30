7443e7a9efd21859bb22239ec9327a3f545d5326
anhpham1504 <phamngochoaianh150406@gmail.com>
2026-05-30 23:25:06 +0700

    fix(campus-map): set updated by from user claims

--- API/API/API/Controllers/CampusMapController.cs
+++ API/API/API/Controllers/CampusMapController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.DTOs;
 using API.Models;
 using API.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
